54cedde983f3c878ffa5f188bc39a9a86668318e
Ryan <hannerr@usf.edu>
2025-03-16 14:48:44 -0400

    Started the creation of the custom class and properties of the Outpost Map.

---
+++ src/main/java/com/eclipsense/voidexpanse/item/customitems/OutpostMap.java
@@ -0,0 +1,9 @@
+package com.eclipsense.voidexpanse.item.customitems;
+
+import net.minecraft.world.item.Item;
+
+public class OutpostMap extends Item {
+    public OutpostMap(Properties properties) {
+        super(properties);
+    }
+}
