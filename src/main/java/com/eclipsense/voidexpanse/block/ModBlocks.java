bfd5343d5cdd168fc8c3a75a14ba118f61d9b9ad
Ryan <hannerr@usf.edu>
2025-03-18 17:25:35 -0400

    xxx

--- src/main/java/com/eclipsense/voidexpanse/block/ModBlocks.java
+++ src/main/java/com/eclipsense/voidexpanse/block/ModBlocks.java
@@ -1,18 +1,12 @@
 package com.eclipsense.voidexpanse.block;
 
 import com.eclipsense.voidexpanse.VoidExpanse;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModBlocks {
@@ -24,7 +18,7 @@ public class ModBlocks {
             "end_stone_voidinium_ore",
                     VoidiniumOreBlock::new, BlockBehaviour.Properties.of()
                             .mapColor(MapColor.COLOR_PURPLE)
-                            .lightLevel((p_50876_) -> {return 7; })
+                            .lightLevel((x) -> {return 7; })
                     .sound(SoundType.STONE));
 
     public static final DeferredBlock<Block> VOIDINIUM_BLOCK = BLOCKS.registerSimpleBlock(
